41210343d7d09f0c041f12f473c4313de53527fd
gaoling0910 <61678448+gaoling0910@users.noreply.github.com>
2020-03-02 15:41:15 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+//コメント
+#コメント
+/*コメント
+コメント
+コメント
+コメント
+*/
+echo "hello fron the TOP!";
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<body>
+  <p>Hello World <?php echo " from PHP"; ?></p>
+</body>
+</html>
